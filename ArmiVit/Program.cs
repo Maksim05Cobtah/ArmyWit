b75a898b5286edafbfd0735d90c97d86168af1cd
cobtahacademy08 <cobtahacademy08@gmail.com>
2026-01-30 16:25:25 +0200

    fix

--- ArmiVit/Program.cs
+++ ArmiVit/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.WebHost.UseUrls("http://localhost:5015");
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
