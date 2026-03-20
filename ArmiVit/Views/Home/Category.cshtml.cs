81d50d28d5f3a1f102ff614026f58a2ba274929d
Sergiy06Cobtah <CobtahMitApp06@gmail.com>
2026-03-20 17:50:03 +0300

    #AV-22 add category page

---
+++ ArmiVit/Views/Home/Category.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ArmiVit.Views.Home
+{
+    public class CategoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
