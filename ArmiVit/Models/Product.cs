d18e63dd15e0072656d1fc51fec67317c5648512
Daniel04Cobtah <CobtahMitApp04>
2026-05-05 17:30:35 +0300

    зробив мяке видалення

--- ArmiVit/Models/Product.cs
+++ ArmiVit/Models/Product.cs
@@ -4,16 +4,16 @@ namespace ProductApi.Models
 {
     public class Product
     {
-        public int Id { get; set; }        // PK
+        public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
-        public int CategoryId { get; set; }   
+        public int CategoryId { get; set; }
         public int Quantity { get; set; }
         public string? ImagePath { get; set; }
         public string? Description { get; set; }
 
-        public Categories Category { get; set; } 
+        public Categories Category { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
     }
 }
-
-
