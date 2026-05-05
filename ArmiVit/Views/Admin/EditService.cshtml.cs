31d5c544fa1826ca022f9a83d97f049ea4696f4f
Sergiy06Cobtah <CobtahMitApp06@gmail.com>
2026-05-05 17:11:56 +0300

    ad a now design

---
+++ ArmiVit/Views/Admin/EditService.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ArmiVit.Views.Admin
+{
+    public class EditServiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
